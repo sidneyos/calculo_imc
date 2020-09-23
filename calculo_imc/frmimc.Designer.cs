7624d75cec9203a4971ff8da09bf3306fb574f50
Sydney <Sydney@DESKTOP-VVI54IM>
2020-09-22 21:09:29 -0300

    calculo imc

---
+++ calculo_imc/frmimc.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace calculo_imc
+{
+    partial class frmimc
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmimc));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.txtaltura = new System.Windows.Forms.TextBox();
+            this.txtimc = new System.Windows.Forms.TextBox();
+            this.btnverificar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(63, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo de IMC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Digite seu peso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Digite sua altura";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(65, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Seu IMC é";
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Location = new System.Drawing.Point(157, 87);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(120, 20);
+            this.txtpeso.TabIndex = 1;
+            // 
+            // txtaltura
+            // 
+            this.txtaltura.Location = new System.Drawing.Point(157, 124);
+            this.txtaltura.Name = "txtaltura";
+            this.txtaltura.Size = new System.Drawing.Size(120, 20);
+            this.txtaltura.TabIndex = 1;
+            // 
+            // txtimc
+            // 
+            this.txtimc.Location = new System.Drawing.Point(157, 161);
+            this.txtimc.Name = "txtimc";
+            this.txtimc.Size = new System.Drawing.Size(120, 20);
+            this.txtimc.TabIndex = 1;
+            // 
+            // btnverificar
+            // 
+            this.btnverificar.BackColor = System.Drawing.Color.Orange;
+            this.btnverificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnverificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnverificar.Location = new System.Drawing.Point(24, 353);
+            this.btnverificar.Name = "btnverificar";
+            this.btnverificar.Size = new System.Drawing.Size(75, 23);
+            this.btnverificar.TabIndex = 3;
+            this.btnverificar.Text = "Verificar";
+            this.btnverificar.UseVisualStyleBackColor = false;
+            this.btnverificar.Click += new System.EventHandler(this.btnverificar_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.BackColor = System.Drawing.Color.Orange;
+            this.btnsair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsair.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsair.Location = new System.Drawing.Point(222, 353);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 3;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = false;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.BackColor = System.Drawing.Color.Orange;
+            this.btnlimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpar.Location = new System.Drawing.Point(121, 353);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpar.TabIndex = 3;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = false;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::calculo_imc.Properties.Resources.imc;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 204);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmimc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 388);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnverificar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtimc);
+            this.Controls.Add(this.txtaltura);
+            this.Controls.Add(this.txtpeso);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmimc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo IMC";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.TextBox txtaltura;
+        private System.Windows.Forms.TextBox txtimc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnverificar;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Button btnlimpar;
+    }
+}
+
---
+++ calculo_imc/frmimc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculo_imc
+{
+    public partial class frmimc : Form
+    {
+        public frmimc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnverificar_Click(object sender, EventArgs e)
+        {
+            double peso, altura, imc;
+            {
+                /*Declaração de variáveis variável recebe conteúdo do textbox*/
+                
+
+                peso = Convert.ToDouble(txtpeso.Text);
+                altura = Convert.ToDouble(txtaltura.Text);
+                imc = peso / (altura * altura);
+
+                /* textbox recebe conteúdo da variável imc = resultado do cálculo definição de utilização de 2 casas decimais*/
+                txtimc.Text = imc.ToString("0.00");
+
+                /*Condições de acordo com os resultados dos cálculos Serão exibidas mensagens, 
+                 * configuração das messagebox, (mensagens, botões, ícones) como são várias condições temos um encadeamento de ifs*/
+
+                if (imc < 18.49) MessageBox.Show("SITUAÇÃO: Você está abaixo do Peso", "Cálculo de IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                else if (imc < 24.99) MessageBox.Show("SITUAÇÃO: Você está com Peso dentro da Normalidade", "Cálculo de IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                else if (imc < 29.99) MessageBox.Show("SITUAÇÃO: Você está acima do Peso", "Cálculo de IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                else if (imc < 34.99) MessageBox.Show("Atenção!Você está com Obesidade Grau I", "Cálculo de IMC", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                else if (imc < 39.99) MessageBox.Show("Atenção!Você está com Obesidade Grau II (severa)", "Cálculo de IMC", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+
+                else MessageBox.Show("Atenção!Você está com grau de obesidade Grau III (mórbida)", "Cálculo de IMC", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtpeso.Text = "";
+            txtaltura.Text = "";
+            txtimc.Text = "";
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
